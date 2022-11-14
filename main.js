eb5f48cc69fd7cc96aa4f2670c7c6f3721c320b6
Luiza Campello <lumcampello@gmail.com>
2022-11-14 16:23:39 -0300

    base files commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function addCSSFile () {
+    const cssLink = document.createElement('link');
+   
+    cssLink.rel = 'stylesheet';
+    cssLink.type = 'text/css';
+    cssLink.href = 'style.css';
+  
+    document.head.appendChild(cssLink);
+}
+
+addCSSFile();
