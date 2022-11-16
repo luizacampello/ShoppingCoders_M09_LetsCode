f1a5f33636452728ccd129dec098d97f9a1b0cb7
Marco Antonio <cootoonhoo@gmail.com>
2022-11-16 11:26:19 -0300

    Adicionando a estrutura HTML - Cards

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const MockStore = 
+{
+    category : 'Alimentação',
+    name: 'McDonalds'
+}
+
+const MockCategory = 
+{
+    categoryName : 'Alimentação',
+    categoryId: 001
+}
+
 function addCSSFile () {
     const cssLink = document.createElement('link');
    
@@ -9,3 +21,54 @@ function addCSSFile () {
 }
 
 addCSSFile();
+
+const divCardsContainer = document.createElement('div');
+divCardsContainer.setAttribute('id','CardsContainer');
+
+createCategoryCard(MockCategory);
+createStoreCard(MockStore);
+
+document.body.appendChild(divCardsContainer) // TO DO: Mudar para a div do container geral quando for feito o merge de todas as branches
+
+function createCategoryCard(object){
+    const divCardContent = document.createElement('div');
+    divCardContent.classList.add('card-content');
+
+    const categoryId = document.createElement('h2');
+    categoryId.textContent = object.categoryId;
+    
+    const categoryName = document.createElement('h1');
+    categoryName.textContent = object.categoryName;
+
+    const cardEditButton = document.createElement('button');
+    cardEditButton.textContent = 'Editar';
+    cardEditButton.classList.add('card-button')
+    
+    const cardStoresButton = document.createElement('button');
+    cardStoresButton.textContent = 'Lojas';
+    cardStoresButton.classList.add('card-button')
+    
+    divCardContent.appendChild(categoryId);
+    divCardContent.appendChild(categoryName);
+    divCardContent.appendChild(cardStoresButton);
+    divCardContent.appendChild(cardEditButton);
+
+    divCardsContainer.appendChild(divCardContent);
+}
+
+function createStoreCard(object){
+    const divCardContent = document.createElement('div');
+    divCardContent.classList.add('card-content');
+
+    const storeCategory = document.createElement('h2');
+    storeCategory.textContent = object.category;
+    
+    const storeName = document.createElement('h1');
+    storeName.textContent = object.name;
+
+    divCardContent.appendChild(storeCategory);
+    divCardContent.appendChild(storeName);
+
+    divCardsContainer.appendChild(divCardContent);
+}
+
