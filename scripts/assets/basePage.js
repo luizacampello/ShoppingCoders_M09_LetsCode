9243e8701aba26cb0ddd0068d719258a4c31f77c
Marco Antonio <cootoonhoo@gmail.com>
2022-11-25 14:29:30 -0300

    Revert "Takeshinishikawa4"

--- scripts/assets/basePage.js
+++ scripts/assets/basePage.js
@@ -1,49 +1,50 @@
 window.basePage = {
-	createMainContainer: () =>{
-		const searchContainer = elementFactory.createHtmlTag(
-			"div",
-			"searchContainer",
-			"searchContainer"
-		);
-		const storesContainer = elementFactory.createHtmlTag(
-			"div",
-			"innerContainer",
-			"storesContainer"
-		);
-		const categoriesContainer = elementFactory.createHtmlTag(
-			"div",
-			"innerContainer",
-			"categoriesContainer"
-		);
-		const popUpContainer = elementFactory.createHtmlTag(
-			"div",
-			"popUpContainer",
-			"popUpContainer"
-		);
-		const mainContainer = elementFactory.createHtmlTag(
-			"div",
-			"mainContainer",
-			"mainContainer"
-		);
+    createMainContainer: () =>{
+        const searchContainer = elementFactory.createHtmlTag(
+            "div",
+            "searchContainer",
+            "searchContainer"
+        );
+        const storesContainer = elementFactory.createHtmlTag(
+            "div",
+            "innerContainer",
+            "storesContainer"
+        );
+        const categoriesContainer = elementFactory.createHtmlTag(
+            "div",
+            "innerContainer",
+            "categoriesContainer"
+        );
+        const popUpContainer = elementFactory.createHtmlTag(
+            "div",
+            "popUpContainer",
+            "popUpContainer"
+        );
+        const mainContainer = elementFactory.createHtmlTag(
+            "div",
+            "mainContainer",
+            "mainContainer"
+        );
 
-		mainContainer.appendChild(popUpContainer);
-		mainContainer.appendChild(searchContainer);
-		mainContainer.appendChild(storesContainer);
-		mainContainer.appendChild(categoriesContainer);
-		document.body.appendChild(mainContainer);
-	},
+        mainContainer.appendChild(popUpContainer);
+        mainContainer.appendChild(searchContainer);
+        mainContainer.appendChild(storesContainer);
+        mainContainer.appendChild(categoriesContainer);
 
-	addFooter: () => {
-		const footer = elementFactory.createHtmlTag("footer", "footer", "footer");
-		const title = elementFactory.createHtmlTagAndSetContent("p", "Categorias", "footer-title");
-		const footerList = elementFactory.createHtmlTagAndSetContent("ul", "", "footer-list");
+        document.body.appendChild(mainContainer);
+    },
 
-		footer.appendChild(title);
-		footer.appendChild(footerList);
-		infra.refreshFooter();
+    addFooter: () => {
+        const footer = elementFactory.createHtmlTag("footer", "footer", "footer");
+        const title = elementFactory.createHtmlTagAndSetContent("p", "Categorias", "footer-title");
+        const footerList = elementFactory.createHtmlTagAndSetContent("ul", "", "footer-list");
+
+        footer.appendChild(title);
+        footer.appendChild(footerList);
+        infra.refreshFooter(); 
 		document.body.appendChild(footer);
-		infra.addFooterCategorySearchEvent();
-	},
+        infra.addFooterCategorySearchEvent();
+    },
 
 	notification: { //TODO
         timer: null,
@@ -71,56 +72,56 @@ window.basePage = {
 
     addHeader: () => {
         const body = document.querySelector('body');
-
+     
         const header = document.createElement('header');
-
+     
         const headerNav = document.createElement('nav');
-
+     
         const logo = elementFactory.createHtmlTag('div', 'logo');
-
+     
         const logoMenu = elementFactory.createHtmlTag('div', 'HeaderElements');
 
         const logoImgWeb = elementFactory.newImage('/imgs/logoWeb.png', 'LogoWeb', 'LogoWeb', 'logo completa do Shopping Coders');
         const logoImgMobile = elementFactory.newImage('/imgs/logoMobile.png', 'LogoMobile', 'LogoMobile', 'logo reduzida do Shopping Coders');
-
+             
         const iconeMenu = document.createElement('span');
         iconeMenu.className = 'material-symbols-outlined';
         iconeMenu.textContent = 'menu';
         iconeMenu.id = 'iconeMenu';
         iconeMenu.addEventListener("click", basePage.clickMenu);
-
+    
         const menu = elementFactory.createHtmlTag('div', 'menu','menu');
-
+    
         const lojas = elementFactory.createHtmlTag('div', 'lojas');
-
+     
         const lojash3 = elementFactory.createHtmlTagAndSetContent('h3', 'Lojas', 'linkStoreContainer');
-
+    
         const lojasUl = elementFactory.createHtmlTag('ul', 'storeHeader', 'storeHeader');
-
+    
         const lojasLi1 = elementFactory.createHtmlTagAndSetContent('li', '+Nova Loja', 'linkPopupNewStore');
-
+     
         const lojasLi2 = elementFactory.createHtmlTagAndSetContent('li', 'Todas as Lojas', 'linkStores');
-
+     
         lojasUl.appendChild(lojasLi1);
         lojasUl.appendChild(lojasLi2);
         lojas.appendChild(lojash3);
         lojas.appendChild(lojasUl);
-
+     
         const categorias = elementFactory.createHtmlTag('div', 'categorias');
-
+     
         const categoriash3 = elementFactory.createHtmlTagAndSetContent('h3', 'Categorias', 'linkCategoryContainer');
-
+    
         const categoriasUl = elementFactory.createHtmlTag('ul', 'categoryHeader', 'categoryHeader');
-
+    
         const categoriasLi1 = elementFactory.createHtmlTagAndSetContent('li', '+Nova Categoria', 'linkPopupNewCategory');
-
+     
         const categoriasLi2 = elementFactory.createHtmlTagAndSetContent('li', 'Todas as Categorias', 'linkCategories');
-
+     
         categoriasUl.appendChild(categoriasLi1);
         categoriasUl.appendChild(categoriasLi2);
         categorias.appendChild(categoriash3);
         categorias.appendChild(categoriasUl);
-
+     
         logo.appendChild(logoImgMobile);
         logo.appendChild(logoImgWeb);
         menu.appendChild(lojas);
@@ -131,32 +132,32 @@ window.basePage = {
         headerNav.appendChild(menu);
         header.appendChild(headerNav);
         body.appendChild(header);
-
+     
         iconeMenu.addEventListener("click", basePage.clickMenu);
         document.body.onresize = () => basePage.showMenu;
-
+    
     },
-
+    
     showMenu: () => {
        const menu = document.getElementById('menu');
-
+    
         if (document.body.clientWidth > 1155) {
             menu.className = 'menu';
         }
-
+    
         if (document.body.clientWidth < 1155) {
             menu.className = 'menuHide';
         }
     },
-
+    
     clickMenu: () => {
         const menu = document.getElementById('menu');
         const display = window.getComputedStyle(menu, null).display;
-
+    
         if (display == 'none') {
             menu.className = 'menuShow';
         }
-
+    
         if (display == 'flex') {
             menu.className = 'menuHide';
         }
