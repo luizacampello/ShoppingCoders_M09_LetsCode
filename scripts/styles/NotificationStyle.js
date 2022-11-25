feac0812c35480b316c2f5126658a16e927b01b5
Marco Antonio <cootoonhoo@gmail.com>
2022-11-25 08:51:44 -0300

    Criando o notificationStyle

---
+++ scripts/styles/NotificationStyle.js
@@ -0,0 +1,33 @@
+(() => {
+    const style = document.createElement('style');
+    style.innerHTML = `
+    div.notification {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        width: 200px;
+        padding: 20px;
+        text-align: center;
+        background: #e8e8e8;
+        border-radius: 20px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+      }
+      
+      div.notification-error {
+        color: #a14343;
+        box-shadow: 0 0 10px rgba(255, 0, 0, 0.5);
+      }
+      
+      div.notification-success {
+        color: #43a143;
+        box-shadow: 0 0 10px rgba(0, 255, 0, 0.5);
+      }
+      
+      div.notification-info {
+          color: #43a143;
+          box-shadow: 0 0 10px rgba(0, 0, 255, 0.5);
+      }
+    `;
+    document.body.appendChild(style);
+})();
+
